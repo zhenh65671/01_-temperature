a6da75e70411434a8e3ce1c4664d9ee4f9f1c0b9
zhenh65671 <zhenh65671@masseyhigh.school.nz>
2020-02-19 09:55:57 +1300

    help gui

---
+++ 00_help GUI.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import functools import partial   # To prevent unwanted windows
+
+import random
+
+
+class Converter:
+    def __init__(self):
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Foo(root)
+    root.mainloop()
--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,1 +1,1 @@
-print("hello world")
+from tkinter import *
