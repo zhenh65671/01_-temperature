e36f0a777381f8f27483b1a7817b4569e08b0241
zhenh65671 <zhenh65671@masseyhigh.school.nz>
2020-02-21 13:36:42 +1300

    help gui

--- 01_Help_GUIv4.py
+++ 01_Help_GUIv4.py
@@ -11,7 +11,7 @@ class Converter:
         background_color = "light blue"
 
         # Converter Main Screen GUI
-        self.converter_frame = Frame(width=600, height=600, bg=background_color,
+        self.converter_frame = Frame(width=300, height=300, bg=background_color,
                                     pady=10)
         self.converter_frame.grid()
 
@@ -29,10 +29,53 @@ class Converter:
         self.help_button.grid(row=1)
 
     def help(self):
-        print("you asked for help"
-         get_help =Help(self)
-         get_help.help_text.configure(text="Help text goes here")
+        print("you asked for help")
+        get_help =Help(self)
+        get_help.help_text.configure(text="Help text goes here")
+
 
 
 class Help:
-        def__in
+     def __init__(self, partner):
+         background = "orange"
+
+         #disable help button
+         partner.help_button.config(state=DISABLED)
+
+         # Sets up child window  (ie: help box)
+         self.help_box = Toplevel()
+
+         # If users press cross at top, closes help and 'releases' help button
+         self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+
+         # Set up GUI Frame
+         self.help_frame = Frame(self.help_box, bg=background)
+         self.help_frame.grid()
+
+         # Set up Help heading (row 0)
+         self.how_hesding = Label(self.help_frame, text="Help / Instruction",
+                                  font="arial 10 bold", bg=background)
+
+         # Help text (label, row 1)
+         self.help_text =Label(self.help_frame, text="",
+                               justify=LEFT, width=40, bg=background,wrap=250)
+         self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+         self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                   width=10, bg="orange", font="arial 10 bold",
+                              command=partial(self.close_help, partner))
+         self.dismiss_btn.grid(row=2, pady=10)
+
+        def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.distroy()
+
+# main routine
+ if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter()
+    root.mainloop()
