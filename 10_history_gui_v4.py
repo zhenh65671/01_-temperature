2ba747960c420d461a36c05c6045d7982cfc846b
zhenh65671 <61208218+zhenh65671@users.noreply.github.com>
2020-03-20 13:15:32 +1300

    2

--- 10_history_gui_v4.py
+++ 10_history_gui_v4.py
@@ -34,16 +34,16 @@ class Converter:
         # history Button (row 1)
         self.history_button = Button(self.converter_frame, text="History",
                                      font=("Arial", "14"),
-                                     padx=10, pady=10, command=self.history)
+                                     padx=10, pady=10,
+                                     command=lambda: self.history(self.all_calc_list))
         self.history_button.grid(row=1)
 
-    def history(self):
-        get_history = history(self)
-        get_history.history_text.configure(text="History text goes here")
+    def history(self, calc_history):
+        History(self, calc_history)
 
 
-class history:
-    def __init__(self, partner):
+class History:
+    def __init__(self, partner, calc_history):
 
         background = "orange"
 
@@ -55,6 +55,7 @@ class history:
 
         # If users press cross at top, closes history and 'releases' history button
         self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+
 
         # Set up GUI Frame
         self.history_frame = Frame(self.history_box, width=300, bg=background)
@@ -78,6 +79,25 @@ class history:
         self.history_text.grid(row=1)
 
         # History output goes here.. (row 2)
+
+        # Generate string from list of calculation...
+        history_string = ""
+
+        if len(calc_history) >= 7:
+            for item in range(0,7):
+                history_string += calc_history[len(calc_history)
+                                               - item - 1]+"\n"
+
+        else:
+            for item in calc_history:
+                history_string += calc_history[len(calc_history) -
+                                               calc_history.index(item) - 1] + "\n"
+                self.history_text.config(text="Here is your calculation ")
+
+        # Label to display calculation history to user
+        self.calc_label = Label(self.history_frame, text=history_string,
+                                bg=background, font="Arial 12", justify=LEFT)
+        self.calc_label.grid(row=2)
 
         # Export / Dismiss Button Frame (row 3)
         self.export_dismiss_frame = Frame(self.history_frame)
